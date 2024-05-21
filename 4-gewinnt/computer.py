9934655cdaea26c3ccd05eb342de7221fe7c8cd8
Sidney Krause <sid.krause.23@heilbronn.dhbw.de>
2024-05-21 16:39:53 +0200

    Formatting minimax function

--- 4-gewinnt/computer.py
+++ 4-gewinnt/computer.py
@@ -13,46 +13,6 @@ class Computer:
 
         self.bitboard = dict()
 
-    def minimax(self, board: Board, maximize: bool, alpha: int, beta: int, depth: int) -> int:
-        game_over, winner = board.is_game_over()
-        if game_over:
-            if winner == 0:
-                return 0
-            else:
-                score = 50 * (1 if winner == 1 else -1)
-                if board not in self.bitboard.keys():
-                    self.bitboard[board] = score
-                return self.bitboard[board]
-        elif depth == 0:
-            return self.eval_field(board)
-
-        if board in self.bitboard:
-            print("cache hit", len(self.bitboard))
-            return self.bitboard[board]
-
-        if maximize:
-            max_eval = -42
-            for move in board.get_possible_moves():
-                next_board = Board(board.field.copy())
-                next_board.place_marker(move)
-                score = self.bitboard[next_board] if next_board in self.bitboard.keys() else self.minimax(board=next_board, maximize=False, alpha=alpha, beta=beta, depth=depth-1)
-                max_eval = max(max_eval, score)
-                alpha = max(alpha, score)
-                if beta <= alpha:
-                    break
-            return max_eval
-        else:
-            min_eval = 42
-            for move in board.get_possible_moves():
-                next_board = Board(board.field.copy())
-                next_board.place_marker(move)
-                score = self.bitboard[next_board] if next_board in self.bitboard.keys() else self.minimax(board=next_board, maximize=True, alpha=alpha, beta=beta, depth=depth-1)
-                min_eval = min(min_eval, score)
-                beta = min(beta, score)
-                if beta <= alpha:
-                    break
-            return min_eval
-
     def calculate_move(self) -> int:
         """
         Works towards finding the optimal move in the given situation for the computer.
@@ -67,7 +27,7 @@ class Computer:
             for move in self.board.get_possible_moves():
                 next_board = Board(self.board.field.copy())
                 next_board.place_marker(move)
-                score = self.minimax(board=next_board, maximize=False, alpha=-42, beta=42, depth=5)
+                score = self.minimax(board=next_board, maximize=False, alpha=-42, beta=42, depth=4)
                 if score > max_score:
                     max_score = score
                     best_move = move
@@ -76,11 +36,59 @@ class Computer:
             for move in self.board.get_possible_moves():
                 next_board = Board(self.board.field.copy())
                 next_board.place_marker(move)
-                score = self.minimax(board=next_board, maximize=True, alpha=-42, beta=42, depth=5)
+                score = self.minimax(board=next_board, maximize=True, alpha=-42, beta=42, depth=4)
                 if score < min_score:
                     min_score = score
                     best_move = move
         return best_move
+
+    def minimax(self, board: Board, maximize: bool, alpha: int, beta: int, depth: int) -> int:
+        game_over, winner = board.is_game_over()
+
+        # Check if board configuration is already cached
+        if board in self.bitboard.keys():
+            print("cache hit", len(self.bitboard))
+            return self.bitboard[board]
+
+        # Scenario 1: game is over
+        if game_over:
+            if winner == 0:
+                move_evaluation = 0
+            else:
+                move_evaluation = 50 * (1 if winner == 1 else -1)
+        # Scenario 2: depth exceeded, evaluate position using heuristics
+        elif depth == 0:
+            move_evaluation = self.eval_field(board)
+        # Scenario 3: Minimax
+        elif maximize:
+            max_eval = -42
+            for move in board.get_possible_moves():
+                next_board = Board(board.field.copy())
+                next_board.place_marker(move)
+                score = self.bitboard[next_board] if next_board in self.bitboard.keys() else self.minimax(
+                    board=next_board, maximize=False, alpha=alpha, beta=beta, depth=depth - 1)
+                max_eval = max(max_eval, score)
+                alpha = max(alpha, score)
+                if beta <= alpha:
+                    break
+            move_evaluation = max_eval
+        else:
+            min_eval = 42
+            for move in board.get_possible_moves():
+                next_board = Board(board.field.copy())
+                next_board.place_marker(move)
+                score = self.bitboard[next_board] if next_board in self.bitboard.keys() else self.minimax(
+                    board=next_board, maximize=True, alpha=alpha, beta=beta, depth=depth - 1)
+                min_eval = min(min_eval, score)
+                beta = min(beta, score)
+                if beta <= alpha:
+                    break
+            move_evaluation = min_eval
+
+        # Cache board configuration with fitting score
+        self.bitboard[board] = move_evaluation
+
+        return self.bitboard[board]
 
     def eval_field(self, board: Board) -> int:
         """
@@ -93,19 +101,19 @@ class Computer:
         # Scenario 1: four in a row
         for y in range(6):
             for x in range(4):
-                _, board_slice = board.is_4_straight_connected(x, y, horizontal=True)
+                board_slice = board.is_4_straight_connected(x, y, horizontal=True)[1]
                 score += self.heuristic_evaluation_of(board_slice)
 
         # Scenario 2: four in a column
         for y in range(3):
             for x in range(7):
-                _, board_slice = board.is_4_straight_connected(x, y, horizontal=False)
+                board_slice = board.is_4_straight_connected(x, y, horizontal=False)[1]
                 score += self.heuristic_evaluation_of(board_slice)
 
         # Scenario 3: four diagonally
         for x in range(4):
             for y in range(3):
-                _, board_slice = board.is_4_diagonal_connected(x, y, high_to_low=False)
+                board_slice = board.is_4_diagonal_connected(x, y, high_to_low=False)[1]
                 score += self.heuristic_evaluation_of(board_slice)
                 score += self.heuristic_evaluation_of(board_slice)
 
@@ -113,7 +121,7 @@ class Computer:
 
     def heuristic_evaluation_of(self, board_slice: tuple) -> int:
         """
-        Gives an heuristic evaluation of a small selection
+        Gives a heuristic evaluation of a small selection
         :param board_slice: the selection from the board
         :return: a score
         """
